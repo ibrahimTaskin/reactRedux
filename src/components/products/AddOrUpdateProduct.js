bb659be2e5782e47231d90c711c8806f413bd3f2
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2020-10-12 19:46:58 +0300

    Add React Hooks

---
+++ src/components/products/AddOrUpdateProduct.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import {getCategories} from  "../../redux/actions/categoryActions"
+import {saveProduct} from  "../../redux/actions/productActions"
+
+function AddOrUpdateProduct({
+  products,
+  categories,
+  getProducts,
+  getCategories,
+  saveProduct,
+  history,
+  ...props
+}) {
+  //product state ini setProcut ile set edicem --> setState yerine
+  const [product, setProduct] = useState({ ...props.product });
+
+  useEffect(() => {
+    if (categories.length === 0) {
+      //hiö çağırılmamışsa getir.
+      getCategories();
+    }
+    setProduct({ ...props.product });
+  }, [props.product]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setProduct((previousProduct) => ({
+      ...previousProduct,
+      [name]: name === "categoryId" ? parseInt(value, 10) : value,
+    }));
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    saveProduct(product).then(() => {
+      history.push("/");
+    });
+  }
+}
+
+const mapDispatchToProps = {
+  getCategories,
+  saveProduct,
+};
+
+export default connect(mapDispatchToProps)(AddOrUpdateProduct);
